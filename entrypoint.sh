42a5fa2d4921a31e6d98bb8b341c12c098ac2738
Vương Tuấn Vinh <vtvinh24@gmail.com>
2025-09-15 09:53:29 +0700

    Update entrypoint.sh, now tracks container instances and reports to a dummy server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+DIR=$(pwd)
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+OSINFO=$(uname -a)
+curl -X POST https://demo-ghcr.requestcatcher.com/ \
+  -H "Content-Type: application/json" \
+  -d "{\"dir\": \"$DIR\", \"timestamp\": \"$TIMESTAMP\", \"os\": \"$OSINFO\"}"
+
 if [ -f main.h ]; then
   ls
   echo "success"
